75c8be15431d999f56ef5a24f38927ef13f2a3ac
G384362023 <238436@st.takushoku-u.ac.jp>
2024-07-02 12:45:32 +0900

    課題終了

--- gourmet.js
+++ gourmet.js
@@ -200,14 +200,89 @@ let data = {
 };
 
 /////////// 課題3-2 ここからプログラムを書こう
+let kazu=0;
 
+
+let kekka = 1;
 mise_data(0);
 mise_data(1);
+let div = document.querySelector("div#result");
+let p = document.createElement('p');
+let h3 = document.createElement('h3');
+div.insertAdjacentElement('beforebegin',p);
+p.insertAdjacentElement('beforeend',h3);
+h3.textContent = 'グルメ情報(検索結果は'+kazu+'件です)';
 
 function mise_data(i) {
-  console.log(data.results.shop[i].name);
-  console.log(data.results.shop[i].station_name);
-  console.log(data.results.shop[i].address);
-  console.log(data.results.shop[i].budget.name);
-  console.log(data.results.shop[i].open);
+  
+  let div = document.querySelector("div#result");
+
+  let li = document.createElement('li');
+  li.textContent = '検索結果'+kekka+'件目';
+  div.insertAdjacentElement('beforeend',li);
+  let table = document.createElement("table");
+  div.insertAdjacentElement('beforeend',table);
+  
+  let caption = document.createElement("caption");
+  table.insertAdjacentElement('beforeend',caption);
+  
+  let thead = document.createElement("thead");
+  caption.insertAdjacentElement('afterend',thead);
+
+  let tr = document.createElement("tr");
+  thead.insertAdjacentElement('beforeend', tr);
+
+  let th = document.createElement('th');
+  let th0 = document.createElement('th');
+  th.setAttribute('scope','col');
+  th0.setAttribute('scope','col');
+  tr.insertAdjacentElement('beforeend',th);
+  th.insertAdjacentElement("afterend",th0)
+  th.textContent = '名前';
+  th0.textContent = data.results.shop[i].name;
+
+  let tbody = document.createElement('tbody');
+  thead.insertAdjacentElement('afterend', tbody);
+
+  let tr1 = document.createElement('tr');
+  let tr2 = document.createElement('tr');
+  let tr3 = document.createElement('tr');
+  let tr4 = document.createElement('tr');
+  tbody.insertAdjacentElement('beforeend', tr1);
+  tr1.insertAdjacentElement('afterend',tr2);
+  tr2.insertAdjacentElement('afterend',tr3);
+  tr3.insertAdjacentElement('afterend',tr4);
+
+  let th1 = document.createElement('th');
+  let th2 = document.createElement('th');
+  let th3 = document.createElement('th');
+  let th4 = document.createElement('th');
+  th1.setAttribute('scope','col');
+  th2.setAttribute('scope','col');
+  th3.setAttribute('scope','col');
+  th4.setAttribute('scope','col');
+  th1.textContent = '最寄り駅';
+  th2.textContent = '住所';
+  th3.textContent = '予算';
+  th4.textContent = '営業時間';
+  tr1.insertAdjacentElement('beforeend',th1);
+  tr2.insertAdjacentElement('beforeend',th2);
+  tr3.insertAdjacentElement('beforeend',th3);
+  tr4.insertAdjacentElement('beforeend',th4);
+
+  let td1 =  document.createElement('td');
+  let td2 =  document.createElement('td');
+  let td3 =  document.createElement('td');
+  let td4 =  document.createElement('td');
+  td1.textContent = data.results.shop[i].station_name;
+  td2.textContent = data.results.shop[i].address;
+  td3.textContent = data.results.shop[i].budget.name;
+  td4.textContent = data.results.shop[i].open;
+  th1.insertAdjacentElement('afterend',td1);
+  th2.insertAdjacentElement('afterend',td2);
+  th3.insertAdjacentElement('afterend',td3);
+  th4.insertAdjacentElement('afterend',td4);
+
+  kekka++;
+  kazu++;
 }
